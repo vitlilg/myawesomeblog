c934c760599dbfc503a5a92ffa55aef183c0a7f5
Vitaliy Grachev <vitlilg@gmail.com>
2021-11-04 14:06:48 +0200

    Add Blog App

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=30)
+    blog_date = models.DateTimeField()
+    blog_text = models.TextField()
+    blog_image = models.ImageField(upload_to='blog_images/')
